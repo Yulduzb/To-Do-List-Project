c16c2c2205aa1cc382c04e87d7374349f619ae00
Yulduz Buranbaeva <130300539+Yulduzb@users.noreply.github.com>
2023-08-06 17:05:12 +0300

    update responsive width

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-    const inputBox = document.getElementById('input-box');
+   
+   const inputBox = document.getElementById('input-box');
     const listContainer = document.getElementById('list-container');
 
 
